b94e0a3bf2671e2e2694c8110ba1b264b9ef24f3
Mario Fink <mf@mariofink.de>
2021-02-18 22:32:46 +0100

    sort recipe list alphabetically

--- .eleventy.js
+++ .eleventy.js
@@ -54,6 +54,14 @@ module.exports = function (eleventyConfig) {
     return content;
   });
 
+  eleventyConfig.addCollection("rezepteAlphabetisch", (collection) =>
+  collection.getFilteredByGlob("./src/rezept/**/*.md").sort((a, b) => {
+    if (a.data.title > b.data.title) return 1;
+    else if (a.data.title < b.data.title) return -1;
+    else return 0;
+  })
+);
+
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
   return {
